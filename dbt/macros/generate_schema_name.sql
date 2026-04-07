ab9ae97add369ef2ad9544c34227a64ebdfa0f5f
TaploBart <bartosz1.rzepinski@gmail.com>
2026-04-07 19:46:30 +0200

    Initial commit

--- dbt/macros/generate_schema_name.sql
+++ dbt/macros/generate_schema_name.sql
@@ -1,4 +1,3 @@
-{# Use literal schema names (e.g. silver) instead of <target_schema>_silver #}
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- if custom_schema_name is none -%}
         {{ target.schema }}
